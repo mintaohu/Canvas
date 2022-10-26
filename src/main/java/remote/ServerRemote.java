e4bcca2f021c67ae4c9e620b959cc7b1c26d1dea
ShangjingJiang <82994542+jxela@users.noreply.github.com>
2022-10-26 12:42:05 +0800

    Update ServerRemote.java

--- src/main/java/remote/ServerRemote.java
+++ src/main/java/remote/ServerRemote.java
@@ -2,13 +2,14 @@ package remote;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 public interface ServerRemote extends Remote {
 
-    public void addUser(String service, String username) throws RemoteException;
+    public boolean addUser(String service, String username) throws RemoteException;
     public void joinRequest(String username) throws RemoteException;
     public void broadcastImg(byte[] data) throws RemoteException;
-    public String[] displayUsers() throws RemoteException;
+    public ArrayList<String> displayUsers() throws RemoteException;
     public void exitBoard(String username) throws RemoteException;
     public void closeBoard() throws RemoteException;
 
